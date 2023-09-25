1c140dc173b885193ba5c2951f51a22da57f364d
Danryanh7 <danielryanh7@gmail.com>
2023-09-24 20:57:03 -0400

    organized a bit more

--- phase1_task3.m
+++ phase1_task3.m
@@ -1,13 +1,17 @@
-% First Commit
+% 3.1
 filename = 'test.wav';
 [y, Fs] = audioread(filename); % Where y and Fs are the audio data and sampling frequency respectively
 
+% 3.2
 % y is a matrix where each column is a channel and each row is a sample
 [numRows, numChannels] = size(y);
 
 if numChannels > 1
-    monoSignal = sum(y, 2) % Sums columns along the second dimension (1st axis)
+    monoSignal = sum(y, 2); % Sums columns along the second dimension (1st axis)
 else
     monoSignal = y;
 end
+
+% 3.3
+sound(monoSignal, Fs);
 
