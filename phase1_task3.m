419f1e5aeef918f74ea72b4cc44b4acecc6d1343
Danryanh7 <danielryanh7@gmail.com>
2023-09-24 20:54:01 -0400

    reads audio and compresses if channels > 1

--- phase1_task3.m
+++ phase1_task3.m
@@ -1,1 +1,13 @@
 % First Commit
+filename = 'test.wav';
+[y, Fs] = audioread(filename); % Where y and Fs are the audio data and sampling frequency respectively
+
+% y is a matrix where each column is a channel and each row is a sample
+[numRows, numChannels] = size(y);
+
+if numChannels > 1
+    monoSignal = sum(y, 2) % Sums columns along the second dimension (1st axis)
+else
+    monoSignal = y;
+end
+
