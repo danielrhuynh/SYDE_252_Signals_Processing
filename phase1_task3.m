bde7c18b57b74351f7f2e2255706ee95241f144b
Danryanh7 <danielryanh7@gmail.com>
2023-10-15 21:29:47 -0400

    removing sample 2, not renaming to prevent confusion

--- phase1_task3.m
+++ phase1_task3.m
@@ -1,11 +1,9 @@
 [sample1_mono, Fs] = signals_processing("sample1.mp3");
-[sample2_mono, Fs] = signals_processing("sample2.mp3");
 [sample3_mono, Fs] = signals_processing("sample3.mp3");
 [sample4_mono, Fs] = signals_processing("sample4.mp3");
 
 % 3.4
 % Write to audio file
 audiowrite('output1.mp3', sample1_mono, Fs);
-audiowrite('output2.mp3', sample2_mono, Fs);
 audiowrite('output3.mp3', sample3_mono, Fs);
 audiowrite('output4.mp3', sample4_mono, Fs);
