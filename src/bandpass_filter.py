87bcd169892cfca692cbac9f5c7e0a27ed04c268
Danryanh7 <danielryanh7@gmail.com>
2023-11-06 17:38:21 -0500

    feature: cooking

--- src/bandpass_filter.py
+++ src/bandpass_filter.py
@@ -1,8 +1,22 @@
 import sys
 import numpy as np
 import scipy
+from scipy.signal import firwin, lfilter
 import matlab.engine
 
 eng = matlab.engine.start_matlab()
+
+fs = 16000
+low_freq = 100
+high_freq = 8000
+
+N = 12
+
+edges = np.linspace(low_freq, high_freq, N+1)
+
+# Da bank
+filters = []
+
+
 
 eng.quit()
